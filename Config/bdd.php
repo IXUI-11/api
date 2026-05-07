40ed89ff9c06d99b4af610f19a8c03eb36004b33
Salaheddine Belhaska <salaheddine.belhaska@sio-carriat.com>
2026-05-07 11:42:09 +0200

    changement du  nom de la  variable pour l'errur de connexion a la base de données

--- Config/bdd.php
+++ Config/bdd.php
@@ -12,9 +12,9 @@ class Bdd{
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
-        } catch (PDOException $e) {
+        } catch (PDOException $erreur) {
             
-            throw new Exception("Erreur de connexion à la base de données : " . $e->getMessage());
+            throw new Exception("Erreur de connexion à la base de données : " . $erreur->getMessage());
             
         }
         
