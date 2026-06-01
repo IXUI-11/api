af0468c97dffa352302e464fac90f53669f751b2
IXUI-11 <lazalasa1990@gmail.com>
2026-06-01 12:55:28 +0200

    feat(mission): implémenter les fonctionnalités CRUD et la gestion du statut des missions
    - Ajout des endpoints de gestion des missions : `ajouterMission`, `modifierMission`, `supprimerMission`
    - Ajout de l'action `changerStatut` pour activer/désactiver une mission
    - Ajout de l'action `missionsPassees` pour récupérer l'historique des missions effectuées par un bénévole
    - Configuration des en-têtes CORS pour autoriser les requêtes GET, POST et DELETE

--- Controller/missions.php
+++ Controller/missions.php
@@ -6,21 +6,50 @@ require_once "../Cservices/missions.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, DELETE");
 
 $bdd = Bdd::getConnection();
 $missionService = new MissionService($bdd);
 
 $action = $_GET['action'] ?? 'all';
 
-
-// Récupérer les missions en fonction de l'action demandée
 if ($action === 'MissionRecent') {
     $missions = $missionService->getMissionRecent();
+    echo json_encode($missions);
+} else if ($action === 'ajouterMission') {
+    $donnes = json_decode(file_get_contents("php://input"), true);
+    $resultat = $missionService->ajouterMission(
+        $donnes['titre'],
+        $donnes['lieu'],
+        $donnes['description'],
+        $donnes['date_mission']
+    );
+    echo json_encode($resultat);
+} else if ($action === 'supprimerMission') {
+    $donnees = json_decode(file_get_contents("php://input"), true);
+    $resultat = $missionService->supprimerMission($donnees['id']);
+    echo json_encode($resultat);
+} else if ($action === 'modifierMission') {
+    $donnees = json_decode(file_get_contents("php://input"), true);
+    $resultat = $missionService->modifierMission(
+        $donnees['id'],
+        $donnees['titre'],
+        $donnees['lieu'],
+        $donnees['description'],
+        $donnees['date_mission']
+    );
+    echo json_encode($resultat);
+} else if ($action === 'missionsPassees') {
+    $id_benevole = $_GET['id_benevole'] ?? null;
+    $missions = $missionService->getMissionsPasseesParBenevole($id_benevole);
+    echo json_encode($missions);
+} else if ($action === 'changerStatut') {
+    $donnees = json_decode(file_get_contents("php://input"), true);
+    $resultat = $missionService->changerStatutMission($donnees['id'], $donnees['actif']);
+    echo json_encode($resultat);
 } else {
     $missions = $missionService->getToutLesMissions();
+    echo json_encode($missions);
 }
-
-echo json_encode($missions);
-
-
 ?>
